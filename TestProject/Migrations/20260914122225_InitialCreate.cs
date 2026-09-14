f245ccfa58160c9ffb0cd108e12ab016f5174c99
IronCrocks <99284866+IronCrocks@users.noreply.github.com>
2026-09-14 16:05:57 +0300

    Добавил InitialCreate migration

---
+++ TestProject/Migrations/20260914122225_InitialCreate.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace TestProject.Migrations
+{
+    /// <inheritdoc />
+    public partial class InitialCreate : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "ReportJobs",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(type: "TEXT", nullable: false),
+                    UserId = table.Column<Guid>(type: "TEXT", nullable: false),
+                    From = table.Column<DateTime>(type: "TEXT", nullable: false),
+                    To = table.Column<DateTime>(type: "TEXT", nullable: false),
+                    CreatedAt = table.Column<DateTime>(type: "TEXT", nullable: false),
+                    Status = table.Column<string>(type: "TEXT", nullable: false),
+                    CountSignIn = table.Column<int>(type: "INTEGER", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_ReportJobs", x => x.Id);
+                    table.CheckConstraint("CK_ReportJobs_CountSignIn_NonNegative", "\"CountSignIn\" >= 0");
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "ReportJobs");
+        }
+    }
+}
